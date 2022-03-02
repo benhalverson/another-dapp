0f9955c34ed043102e83d7d4895e1273a2787f61
WolfieLeader <koren2318@gmail.com>
2022-03-02 20:54:18 +0200

    Git Ignore

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Error = () => {
-	return <div>Error</div>;
+	return <div className="error">This Page Doesn't Exist Please Retrurn To The Home Page</div>;
 };
 
 export default Error;
